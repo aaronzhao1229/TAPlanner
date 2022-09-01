2c7019be480e56837c5dac2f881a15df01028035
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-09-01 12:07:15 +1200

    add regions, sections and tracks table

---
+++ migrations/20220831233653_tracks.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('tracks', (table) => {
+    table.increments('id')
+    table.string('name')
+    table.string('category')
+    table.integer('regionId')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('tracks')
+}
