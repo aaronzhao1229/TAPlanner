1f561b8ea61b8710d5c81be36b8ac46f8d62a7e6
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-01 20:07:53 +1300

    add settimeout and refactor for state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,50 @@
-import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import Nav from './Nav'
 import Header from './Header'
 import Planner from './Planner'
 import Gears from './Gears'
-
+import DelayComponent from './DelayComponent'
 import SingleProfile from './SingleProfile'
 import LandingPage from './LandingPage'
 import Home from './Home'
 import CreateProfile from './CreatProfile'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useCacheUser } from '../auth0-utils'
+import { getUser } from '../apis/user.api'
+import { useDispatch } from 'react-redux'
+import { useAuth0 } from '@auth0/auth0-react'
+import { clearLoggedInUser, updateLoggedInUser } from '../actions/loggedInUser'
 
 const App = () => {
+  useCacheUser()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [table, setTable] = useState([])
   function updateTable(newData) {
     setTable(newData)
   }
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+  // const userInData = useSelector((state) => state.loggedInUser)
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      dispatch(clearLoggedInUser())
+    } else {
+      getAccessTokenSilently()
+        .then((token) => getUser(token))
+        .then((userInDb) => {
+          if (userInDb[0]) {
+            dispatch(updateLoggedInUser(userInDb[0]))
+          } else {
+            console.log('navigate')
+            navigate('/createProfile')
+          }
+        })
+        .catch((err) => console.error(err))
+    }
+  }, [isAuthenticated])
 
   return (
     <div>
@@ -29,8 +57,10 @@ const App = () => {
                 <Home />
               </IfAuthenticated>
               <IfNotAuthenticated>
-                <Header />
-                <LandingPage />
+                <DelayComponent>
+                  <Header />
+                  <LandingPage />
+                </DelayComponent>
               </IfNotAuthenticated>
             </>
           }
