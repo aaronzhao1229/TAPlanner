b1af9bb7cc87c3109bc5e04e15de13a78a199410
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-02 22:12:20 +1300

    add Planner component test

--- client/components/__tests__/Planner.test.jsx
+++ client/components/__tests__/Planner.test.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import AddSection from '../AddSection'
 const { screen, render } = require('@testing-library/react')
 require('@testing-library/jest-dom')
+import userEvent from '@testing-library/user-event'
 
 jest.mock('../AddSection')
-
+AddSection.mockReturnValue(<></>)
 import Planner from '../Planner'
+const fakeFunc = jest.fn()
 
 describe('Planner', () => {
-  it('test if table is displayed', () => {
-    AddSection.mockReturnValue(<></>)
+  it('test if table is displayed', async () => {
     render(
       <Planner
         tableData={{
@@ -27,5 +28,26 @@ describe('Planner', () => {
     )
     const table = screen.getByRole('table')
     expect(table).toHaveTextContent('canterbury')
+  })
+
+  it('test if the table data has been delete after clicking delete button', async () => {
+    render(
+      <Planner
+        tableData={{
+          table: [
+            {
+              day: '1',
+              region: 'canterbury',
+              track: 'Avalanche Peak',
+              category: 'expert',
+              section: 'Arthurs Pass to Peak',
+            },
+          ],
+        }}
+        setTableFunction={{ updateTable: fakeFunc }}
+      />
+    )
+    await userEvent.click(screen.getByRole('button', { name: 'Delete' }))
+    expect(fakeFunc).toHaveBeenCalled()
   })
 })
