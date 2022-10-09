4612e539627915a41e8873275922e4da4465f517
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-10 11:47:09 +1300

    refactor

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="flex flex-col items-center pt-5 pb-5 bg-base-100">
-      <h1 className="text-4xl">Te Araroa Planner</h1>
+      <h1 className="text-4xl text-emerald-700">Te Araroa Planner</h1>
     </div>
   )
 }
