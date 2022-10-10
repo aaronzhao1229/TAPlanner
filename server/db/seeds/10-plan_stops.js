c6bac0df056a19aa1150ad48bd393bd21aab9035
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-10 20:56:42 +1300

    refactor to database

--- server/db/seeds/10-plan_stops.js
+++ server/db/seeds/10-plan_stops.js
@@ -3,5 +3,8 @@
  * @returns { Promise<void> }
  */
 exports.seed = function (knex) {
-  return knex('plan_stops').insert([{ planId: 1, stopId: 1 }])
+  return knex('plan_stops').insert([
+    { planId: 1, stopId: 1 },
+    { planId: 2, stopId: 2 },
+  ])
 }
