f901a48814fe6534d99c76daa0642bf11a645daa
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-24 21:09:06 +1300

    add summary to each category

--- client/components/Category.jsx
+++ client/components/Category.jsx
@@ -5,8 +5,17 @@ import { useSelector, useDispatch } from 'react-redux'
 import { fetchGearsForUser } from '../actions/gears'
 
 export default function Category({ category }) {
+  let allWeight = 0
+  let allPrice = 0
+  let allQuantity = 0
   const user = useSelector((state) => state.loggedInUser)
   const gears = useSelector((state) => state.gears)
+  const targetGears = gears.filter((gear) => gear.categoryId === category.id)
+  targetGears.forEach((gear) => {
+    allWeight += gear.weight
+    allPrice += gear.price
+    allQuantity += gear.quantity
+  })
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchGearsForUser(user.id))
@@ -31,23 +40,28 @@ export default function Category({ category }) {
             </tr>
           </thead>
           <tbody>
-            {gears.length !== 0 &&
-              gears
-                .filter((gear) => gear.categoryId === category.id)
-                .map((gear) => {
-                  return (
-                    <tr key={gear.id}>
-                      <td>{gear.gear}</td>
-                      <td>{gear.description}</td>
-                      <td>{gear.price}</td>
-                      <td>{gear.weight}</td>
-                      <td>{gear.quantity}</td>
-                      <td>
-                        <button className="btn btn-error btn-sm">Delete</button>
-                      </td>
-                    </tr>
-                  )
-                })}
+            {targetGears.map((gear) => {
+              return (
+                <tr key={gear.id}>
+                  <td>{gear.gear}</td>
+                  <td>{gear.description}</td>
+                  <td>{gear.price}</td>
+                  <td>{gear.weight}</td>
+                  <td>{gear.quantity}</td>
+                  <td>
+                    <button className="btn btn-error btn-sm">Delete</button>
+                  </td>
+                </tr>
+              )
+            })}
+            <tr>
+              <td></td>
+              <td></td>
+              <td>{allPrice}</td>
+              <td>{allWeight}</td>
+              <td>{allQuantity}</td>
+              <td></td>
+            </tr>
           </tbody>
         </table>
       </div>
