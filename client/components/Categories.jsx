1a2958384cf567745bea9171ece28f246e5b25cb
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-24 20:57:18 +1300

    refactor

--- client/components/Categories.jsx
+++ client/components/Categories.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Category from './Category'
-import AddGear from './AddGear'
+
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchGearCategoriesForUser } from '../actions/gearCategories'
 import { fetchGearsForUser } from '../actions/gears'
@@ -8,7 +8,6 @@ import { fetchGearsForUser } from '../actions/gears'
 export default function Categories() {
   const user = useSelector((state) => state.loggedInUser)
   const gearCategories = useSelector((state) => state.gearCategories)
-  const gears = useSelector((state) => state.gears)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchGearCategoriesForUser(user.id))
@@ -20,50 +19,7 @@ export default function Categories() {
       {gearCategories.map((category) => {
         return (
           <div key={category.id}>
-            <Category>
-              <div className="pt-5 pl-2 text-lg font-bold">
-                {category.category}
-              </div>
-              <div className="overflow-x-auto">
-                <table
-                  id="geartable"
-                  className="table table-compact w-full border-separate"
-                >
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Description</th>
-                      <th>Price</th>
-                      <th>Weight</th>
-                      <th>Quantity</th>
-                      <th>Delete?</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {gears.length !== 0 &&
-                      gears
-                        .filter((gear) => gear.categoryId === category.id)
-                        .map((gear) => {
-                          return (
-                            <tr key={gear.id}>
-                              <td>{gear.gear}</td>
-                              <td>{gear.description}</td>
-                              <td>{gear.price}</td>
-                              <td>{gear.weight}</td>
-                              <td>{gear.quantity}</td>
-                              <td>
-                                <button className="btn btn-error btn-sm">
-                                  Delete
-                                </button>
-                              </td>
-                            </tr>
-                          )
-                        })}
-                  </tbody>
-                </table>
-              </div>
-              <AddGear />
-            </Category>
+            <Category category={category} />
           </div>
         )
       })}
--- client/components/Category.jsx
+++ client/components/Category.jsx
@@ -1,5 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import AddGear from './AddGear'
+import { useSelector, useDispatch } from 'react-redux'
 
-export default function Category({ children }) {
-  return <div>{children}</div>
+import { fetchGearsForUser } from '../actions/gears'
+
+export default function Category({ category }) {
+  const user = useSelector((state) => state.loggedInUser)
+  const gears = useSelector((state) => state.gears)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchGearsForUser(user.id))
+  }, [])
+
+  return (
+    <div>
+      <div className="pt-5 pl-2 text-lg font-bold">{category.category}</div>
+      <div className="overflow-x-auto">
+        <table
+          id="geartable"
+          className="table table-compact w-full border-separate"
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Weight</th>
+              <th>Quantity</th>
+              <th>Delete?</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gears.length !== 0 &&
+              gears
+                .filter((gear) => gear.categoryId === category.id)
+                .map((gear) => {
+                  return (
+                    <tr key={gear.id}>
+                      <td>{gear.gear}</td>
+                      <td>{gear.description}</td>
+                      <td>{gear.price}</td>
+                      <td>{gear.weight}</td>
+                      <td>{gear.quantity}</td>
+                      <td>
+                        <button className="btn btn-error btn-sm">Delete</button>
+                      </td>
+                    </tr>
+                  )
+                })}
+          </tbody>
+        </table>
+      </div>
+      <AddGear />
+    </div>
+  )
 }
