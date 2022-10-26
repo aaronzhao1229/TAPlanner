62b80636f77aea7f56d687b63f8e6f0fa58dd897
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-26 15:46:20 +1300

    add gears router tests

--- server/routes/__tests__/gears.test.js
+++ server/routes/__tests__/gears.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest')
 const server = require('../../server')
 
-const { getGears, getCategory } = require('../../db/db.gears')
+const {
+  getGears,
+  getCategory,
+  addCategory,
+  addGear,
+} = require('../../db/db.gears')
 
 jest.mock('../../db/db.gears')
 jest.spyOn(console, 'error')
@@ -59,6 +64,65 @@ describe('GET /gears/getGearCategories/:userId', () => {
       .then((res) => {
         expect(res.status).toBe(500)
         expect(console.error).toHaveBeenCalledWith('failure again')
+      })
+  })
+})
+
+describe('POST /gears/addCategoryForUser', () => {
+  it('return all categories after adding', () => {
+    const fakeCategories = [
+      { category: 'bathroom' },
+      { category: 'office' },
+      { category: 'random' },
+    ]
+    addCategory.mockReturnValue(Promise.resolve(fakeCategories))
+    return request(server)
+      .post('/gears/addCategoryForUser')
+      .then((res) => {
+        expect(res.body).toHaveLength(3)
+        expect(res.body[2].category).toBe('random')
+      })
+  })
+
+  it('show error', () => {
+    addCategory.mockImplementation(() =>
+      Promise.reject(new Error('add failure'))
+    )
+    console.error.mockImplementation(() => {})
+    return request(server)
+      .post('/gears/addCategoryForUser')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(console.error).toHaveBeenCalledWith('add failure')
+      })
+  })
+})
+
+describe('POST /gears/addGearForUser', () => {
+  it('return all gears after adding', () => {
+    const fakeGears = [
+      { gear: 'stove', weight: 80 },
+      { gear: 'spork', weight: 15 },
+    ]
+    addGear.mockReturnValue(Promise.resolve(fakeGears))
+    return request(server)
+      .post('/gears/addGearForUser')
+      .then((res) => {
+        expect(res.body).toHaveLength(2)
+        expect(res.body[1].weight).toBe(15)
+      })
+  })
+
+  it('show error', () => {
+    addGear.mockImplementation(() =>
+      Promise.reject(new Error('add gear failure'))
+    )
+    console.error.mockImplementation(() => {})
+    return request(server)
+      .post('/gears/addGearForUser')
+      .then((res) => {
+        expect(res.status).toBe(500)
+        expect(console.error).toHaveBeenCalledWith('add gear failure')
       })
   })
 })
