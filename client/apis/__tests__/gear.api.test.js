f9eed0b6eb9a12d40fbcdf11710526a4d4e2301b
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-26 21:34:45 +1300

    add delete api tests

--- client/apis/__tests__/gear.api.test.js
+++ client/apis/__tests__/gear.api.test.js
@@ -5,6 +5,8 @@ import {
   getGearCategoriesForUser,
   addGearCategoryForUser,
   addGearForUser,
+  deleteGearForUser,
+  deleteCategoryForUser,
 } from '../gear.api'
 
 describe('GearsForUser', () => {
@@ -62,6 +64,42 @@ describe('addGearsForUser', () => {
     return addGearForUser().then((result) => {
       expect(result).toHaveLength(3)
       expect(result[1].gear).toBe('pot')
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+})
+
+describe('deleteGearForUser', () => {
+  it('return all gear data after deleting a gear', () => {
+    const fakeGears = [
+      { gear: 'pillow' },
+      { gear: 'pot' },
+      { gear: 'water filter' },
+    ]
+    const scope = nock('http://localhost')
+      .delete('/gears/deleteGear/2/1')
+      .reply(200, fakeGears)
+    return deleteGearForUser(2, 1).then((result) => {
+      expect(result).toHaveLength(3)
+      expect(result[0].gear).toBe('pillow')
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+})
+
+describe('deleteCategoryForUser', () => {
+  it('return all categories after deleting a category', () => {
+    const fakeCategories = [
+      { category: 'others' },
+      { category: 'bathroom' },
+      { category: 'random' },
+    ]
+    const scope = nock('http://localhost')
+      .delete('/gears/deleteCategory/2/1')
+      .reply(200, fakeCategories)
+    return deleteCategoryForUser(2, 1).then((result) => {
+      expect(result).toHaveLength(3)
+      expect(result[0].category).toBe('others')
       expect(scope.isDone()).toBe(true)
     })
   })
