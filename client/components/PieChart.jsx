b64104348160a2b5bf4d8f45292ebf0800a6ea16
Aaron Zhao <101838207+aaronzhao1229@users.noreply.github.com>
2022-10-27 22:47:15 +1300

    add colors to pie chart

--- client/components/PieChart.jsx
+++ client/components/PieChart.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import 'chart.js/auto'
-import { Pie } from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
 
 export default function PieChart() {
   const pieChartData = {
     labels: [],
-    datasets: [{ label: 'weight', data: [] }],
+    datasets: [
+      {
+        label: 'weight',
+        data: [],
+        backgroundColor: [
+          '#66CC8A',
+          '#377CFB',
+          '#EA5234',
+          '#333C4D',
+          '#3ABFF8',
+          '#36D399',
+          '#FBBD23',
+          '#F87272',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+      },
+    ],
   }
   const categories = useSelector((state) => state.gearCategories)
   const gears = useSelector((state) => state.gears)
@@ -26,7 +47,7 @@ export default function PieChart() {
   return (
     <div className="flex flex-row justify-center">
       <div className="basis-1/4">
-        <Pie data={pieChartData} />
+        <Doughnut data={pieChartData} />
       </div>
     </div>
   )
